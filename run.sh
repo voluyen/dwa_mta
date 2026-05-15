b2efd89737759dd05854690316ff0ba905836d7f
unknown <voluyenpr@gmail.com>
2026-05-15 23:25:49 +0700

    .

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 set -e
 bash download_model.sh
 python -m pip install -r requirements.txt
+mkdir -p /mnt/distillm-2/outputs
 
 # echo "Training DWA-KD for GPT2-Base..."
 # bash scripts/gpt2/span_dwa_kd_gpt2_base.sh
@@ -9,14 +10,14 @@ echo "Training DWA-KD for GPT2-Medium..."
 bash scripts/gpt2_medium/span_dwa_kd_gpt2_medium.sh
 
 # copy folder outputs/gpt2/ to /mnt/distillm-2/outputs/gpt2/ for evaluation and analysis
-cp -r outputs/gpt2/ /mnt/distillm-2/outputs/gpt2/
+cp -r outputs/gpt2/ /mnt/distillm-2/outputs/
 
 # echo "Training DWA-KD for GPT2-XL..."
 # bash scripts/gpt2xl/span_dwa_kd_gpt2xl.sh
 
 echo "Training DWA-KD for TinyLlama..."
 bash scripts/tinyllama/span_dwa_kd_tinyllama.sh
-cp -r outputs/tinyllama/ /mnt/distillm-2/outputs/tinyllama/
+cp -r outputs/tinyllama/ /mnt/distillm-2/outputs/
 
 # echo "Training DWA-KD for OPT..."
 # bash scripts/opt/span_dwa_kd_opt.sh
